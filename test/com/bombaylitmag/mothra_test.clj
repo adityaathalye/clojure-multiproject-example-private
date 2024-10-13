a63a37f65185b37b9c8eb047b038c3437c718cad
Aditya Athalye <adi@evalapply.org>
2024-10-13 19:24:36 +0530

    Initial commit of port of tblm/mothra

---
+++ test/com/bombaylitmag/mothra_test.clj
@@ -0,0 +1,7 @@
+(ns com.bombaylitmag.mothra-test
+  (:require [clojure.test :refer :all]
+            [com.bombaylitmag.mothra :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
