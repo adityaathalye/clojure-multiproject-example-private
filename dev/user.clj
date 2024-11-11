6fe45deb18085ca5d93fad645c4bdd063a324e6b
Aditya Athalye <adi@evalapply.org>
2024-11-11 23:00:48 +0530

    Add djblue/portal to dev dependencies and user.clj

--- dev/user.clj
+++ dev/user.clj
@@ -1,8 +1,11 @@
 (ns user
   (:require [integrant.repl :as ig-repl]
+            [integrant.repl.state :as ig-state]
             [clojure.tools.namespace.repl :as repl]
             [com.bombaylitmag.system.core]
-            [clojure.repl.deps :as repl-deps :refer [add-lib]]))
+            [clojure.repl.deps :as repl-deps :refer [add-lib]]
+            [portal.api :as p]
+            [clojure.reflect :as reflect]))
 
 (ig-repl/set-prep! (constantly (com.bombaylitmag.system.core/config :dev)))
 
@@ -19,6 +22,19 @@
   (halt)
   (reset)
   (reset-all)
+
+  (reflect/reflect ig-state/system)
+
+  ;; Open portal
+  (p/open)
+  (add-tap #'p/submit)
+
+  (p/stop)
+  (p/start)
+
+  (tap> ig-state/system)
+
+  (tap> (.getConnection (get-in ig-state/system [:database/primary :reader])))
 
   (add-lib 'sym {:mvn/version "x.y.z"})
   )
